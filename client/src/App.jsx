17b44dd408a56854b8eb769947966eca80f9a8ad
irsuhail <irsuhail2000@gmail.com>
2025-09-14 13:23:47 +0530

    Fix merge conflicts and update App.jsx and main.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,23 @@
-<<<<<<< HEAD
 import React from 'react'
-import { Route, Routes, useLocation } from 'react-router-dom'
-import Home from './pages/Home'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import AllRooms from './pages/AllRooms'
+import RoomDetails from './pages/RoomDetails'
+import MyBookings from './pages/MyBookings'
+import HotelReg from './components/HotelReg'
 import Layout from './pages/hotelOwner/Layout'
 import Dashboard from './pages/hotelOwner/Dashboard'
 import AddRoom from './pages/hotelOwner/AddRoom'
 import ListRoom from './pages/hotelOwner/ListRoom'
-import HotelReg from './components/HotelReg'
-import { useAppContext } from './context/AppContext'
 import { Toaster } from 'react-hot-toast'
-import AllRooms from './pages/AllRooms'
-import RoomDetails from './pages/RoomDetails'
-import Footer from './components/Footer'
-import MyBookings from './pages/MyBookings'
+import { useAppContext } from './context/AppContext'
 import Loader from './components/Loader'
 
 const App = () => {
-
-  // Check Is Route Starts With Owner
-  const isOwnerPath = useLocation().pathname.includes("owner");
-
-  const { showHotelReg } = useAppContext();
+  const isOwnerPath = useLocation().pathname.includes('owner')
+  const { showHotelReg } = useAppContext()
 
   return (
     <div className='font-inter'>
@@ -29,58 +25,20 @@ const App = () => {
       {!isOwnerPath && <Navbar />}
       {showHotelReg && <HotelReg />}
       <div className='min-h-[70vh]'>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/rooms' element={<AllRooms />} />
-        <Route path='/rooms/:id' element={<RoomDetails />} />
-        <Route path='my-bookings' element={<MyBookings />} />
-        < Route path="/loader/:nextUrl" element={<Loader />} />
-        <Route path="/owner" element={<Layout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="add-room" element={<AddRoom />} />
-          <Route path="list-room" element={<ListRoom />} />
-        </Route>
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/rooms' element={<AllRooms />} />
+          <Route path='/rooms/:id' element={<RoomDetails />} />
+          <Route path='/my-bookings' element={<MyBookings />} />
+          <Route path='/loader/:nextUrl' element={<Loader />} />
+          <Route path='/owner' element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path='add-room' element={<AddRoom />} />
+            <Route path='list-room' element={<ListRoom />} />
+          </Route>
+        </Routes>
       </div>
       <Footer />
-=======
-import { Routes, Route, useLocation } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import AllRooms from "./pages/AllRooms";
-import RoomDetails from "./pages/RoomDetails";
-import MyBookings from "./pages/MyBookings";
-import HotelReg from "./components/HotelReg";
-import Layout from "./pages/hotelOwner/Layout";
-import Dashboard from "./pages/hotelOwner/Dashboard";
-import AddRoom from "./pages/hotelOwner/AddRoom";
-import ListRoom from "./pages/hotelOwner/ListRoom";
-
-
-const App=()=>{
-
-  const isOwnerPath=useLocation().pathname.includes("owner");
-  return (
-    <div>
-      {!isOwnerPath && <Navbar/>}
-      {false && <HotelReg/>}
-      <div className='min-h-[70vh]'>
-         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/rooms' element={<AllRooms/>}/>
-          <Route path='/rooms/:id' element={<RoomDetails/>}/>
-          <Route path='/my-bookings' element={<MyBookings/>}/>
-          <Route path='/owner' element={<Layout/>}>
-          <Route index element={<Dashboard/>}/>
-          <Route path='add-room' element={<AddRoom/>}/>
-          <Route path='list-room' element={<ListRoom/>}/>
-
-          </Route>
-         </Routes>
-      </div>
-      <Footer/>
->>>>>>> d07792d39304ee2987ae2083b0117e9192edd37f
     </div>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-=======
 import { StrictMode } from 'react'
->>>>>>> d07792d39304ee2987ae2083b0117e9192edd37f
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-<<<<<<< HEAD
 import { AppProvider } from './context/AppContext.jsx'
-=======
->>>>>>> d07792d39304ee2987ae2083b0117e9192edd37f
 import { ClerkProvider } from '@clerk/clerk-react'
 
 // Import your Publishable Key
@@ -20,19 +14,13 @@ if (!PUBLISHABLE_KEY) {
 }
 
 createRoot(document.getElementById('root')).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-<<<<<<< HEAD
-    <BrowserRouter>
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </BrowserRouter>
-  </ClerkProvider>
+  <StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <BrowserRouter>
+        <AppProvider>
+          <App />
+        </AppProvider>
+      </BrowserRouter>
+    </ClerkProvider>
+  </StrictMode>
 )
-=======
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  </ClerkProvider>,
-)
->>>>>>> d07792d39304ee2987ae2083b0117e9192edd37f
